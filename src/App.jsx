b24a8d66ba018f43317212b5a821ce94abaf2cf2
Clara Escobar <65355521+Claraescobar123@users.noreply.github.com>
2023-03-08 20:49:59 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+import { useState } from "react";
 
+function App(){
+
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+
+  const errorMessage = validate(email, password);
+  
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Ingresa tu nombre y contraseña para poder ingresar</h1>
+      <form>
+        <input
+          type="text"
+          name="nombre"
+          placeholder="Nombre"
+          value={nombre}
+          onChange={(event) => setNombre(event.target.value)}
+        ></input>
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+        <p>{errorMessage}</p>
+        <button tyoe='submit'>Iniciar Sesión</button>
+      </form>
     </div>
   );
+}
+
+const validate = (nombre, password) => {
+  if(nombre.lenght < 3 ) 
+  return 'debe contener al menos 3 caracteres'
+  if(password < 6)
+  return 'debe contener al menos 6 caracteres'
 }
 
 export default App;
